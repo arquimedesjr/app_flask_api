6f3b0d572ae29d9fe2f61bb8e076bc8251fb0007
Arquimedes <arquimedeslqjunior@gmail.com>
2019-10-30 08:22:23 -0300

    ...

--- professores_api.py
+++ professores_api.py
@@ -8,57 +8,43 @@ from services.professores_service import \
 
 professores_app = Blueprint('professores_app', __name__, template_folder='templates')
 
-professores_db = service_listar()
-
-# ROTAS PARA PROFESSOR
-@professores_app.route("/professores", methods = ["GET"])
+@professores_app.route('/professores', methods=['GET'])
 def listar():
-    return jsonify(professores_db)
+    lista = service_listar()
+    return jsonify(lista)
+
+@professores_app.route('/professores/<int:id>', methods=['GET'])
+def localizar(id):
+    p = service_localiza(id)
+    if p != None:
+        return jsonify(p)
+    return jsonify({'erro':'professor sem nome'}), 400
 
 @professores_app.route('/professores', methods=['POST'])
-def novo_professor():
-    novo_professor = request.json
-    if 'nome' not in novo_professor:
-        return jsonify({'erro': 'professor sem nome'}), 404
-    for professor in professores_db:
-        if professor['id'] == novo_professor['id']:
-            return jsonify({'erro': 'id já utilizada'}), 404
-        else:
-            pass
-    professores_db.append(novo_professor)
-    return jsonify(professores_db)
+def novo():
+    novo_professor = request.get_json()
+    if novo_professor == None:
+        return jsonify({'erro':'dados não informados'}), 400
+    if 'id' not in novo_professor:
+        return jsonify({'erro':'id não informado'}), 400
+    p = service_criar(novo_professor)
+    if p == None:
+        return jsonify({'erro':'professor Já cadastrado'}), 400
+    return jsonify(p)
 
-@professores_app.route('/professores/<int:id_professor>', methods=['GET'])
-def localiza_professor(id_professor):
-    for professor in professores_db:
-        if professor['id'] == id_professor:
-            return jsonify(professor)
-    return jsonify({'erro': 'professor não encontrado'}), 404
+@professores_app.route('/professores/<int:id>', methods=['DELETE'])
+def remover(id):
+    removido = service_remover(id)
+    if removido == 1:
+        return jsonify(removido)
+    return jsonify({'erro':'professor nao encontrado'}), 400
 
-@professores_app.route('/professores/<int:id_professor>', methods=['PUT'])
-def edita_professor(id_professor):
-    dados = request.json
-    if 'nome' not in dados:
-        return jsonify({'erro': 'professor sem nome'}), 404
-    for professor in professores_db:
-        if professor['id'] == id_professor:
-            professor['nome'] = dados['nome']
-            return ""
-    return jsonify({'erro': 'professor não encontrado'}), 404
-
-@professores_app.route('/professores/<int:id_professor>', methods=['DELETE'])
-def deleta_professor(id_professor):
-    cont = 0
-    for professor in professores_db:
-        if professor['id'] == id_professor:
-            del(professores_db[cont])
-            return professores_db
-        else:
-            cont+=1
-    return jsonify({'erro': 'professor não encontrado'}), 404
-
-@professores_app.route('/professores/reseta', methods=['POST'])
-def reseta_professores():
-    while len(professores_db) != 0:
-        del(professores_db[0])
-    return jsonify(professores_db)
+@professores_app.route('/professores/<int:id>', methods=['PUT'])
+def atualiza(id):
+    professor_data = request.get_json()
+    if ('nome' not in professor_data):
+        return jsonify({'erro':'professor sem nome'}), 400
+    atualizado = service_atualiza(id, professor_data['nome'])
+    if atualizado != None:
+        return jsonify(atualizado)
+    return jsonify({'erro':'professor nao encontrado'}), 400
